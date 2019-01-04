30885586d9bf99bc8c0535520f381e7c1e784fef
badikirwan <badikirwan@gmail.com>
2019-01-05 05:14:59 +0700

    Changes in the adapter

--- app/src/main/java/com/badikirwan/sistersacnner/sisterscanner/adapter/BarcodeAdapter.kt
+++ app/src/main/java/com/badikirwan/sistersacnner/sisterscanner/adapter/BarcodeAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.badikirwan.sistersacnner.sisterscanner.R
 import com.badikirwan.sistersacnner.sisterscanner.model.Data
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_row.view.*
 
 class BarcodeAdapter(private val data: List<Data>) : RecyclerView.Adapter<BarcodeAdapter.ViewHolder>() {
@@ -28,6 +29,8 @@ class BarcodeAdapter(private val data: List<Data>) : RecyclerView.Adapter<Barcod
             itemView.tv_nama.text = data.namaLengkap
             itemView.tv_nim.text = data.mhsNim
             itemView.tv_jurusan.text = data.jurusan
+            itemView.tv_angkatan.text = data.tahunAngkatan
+            Glide.with(itemView.context).load(data.fileFoto).into(itemView.img_mhs)
         }
     }
 
