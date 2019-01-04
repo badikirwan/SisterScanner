9bafff437fb04ff4591f810e68ad1e2cf9c07498
badikirwan <badikirwan@gmail.com>
2019-01-04 07:03:57 +0700

    Adding barcode adapter

---
+++ app/src/main/java/com/badikirwan/sistersacnner/sisterscanner/adapter/BarcodeAdapter.kt
@@ -0,0 +1,34 @@
+package com.badikirwan.sistersacnner.sisterscanner.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.badikirwan.sistersacnner.sisterscanner.R
+import com.badikirwan.sistersacnner.sisterscanner.model.Data
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class BarcodeAdapter(private val data: List<Data>) : RecyclerView.Adapter<BarcodeAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_row, parent, false
+            )
+        )
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: BarcodeAdapter.ViewHolder, position: Int) {
+        holder.bindItem(data[position])
+    }
+
+    class ViewHolder(itemView: View ): RecyclerView.ViewHolder(itemView) {
+        fun bindItem(data: Data) {
+            itemView.tv_nama.text = data.namaLengkap
+            itemView.tv_nim.text = data.mhsNim
+            itemView.tv_jurusan.text = data.jurusan
+        }
+    }
+
+}
